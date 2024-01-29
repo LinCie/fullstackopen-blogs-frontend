f78b1b5f05e15014e96d549ddd5a874898fdd377
LinCie <aldiofernanda@protonmail.com>
2024-01-29 11:12:42 +0700

    update plus icon

--- src/components/blog/NewBlog.jsx
+++ src/components/blog/NewBlog.jsx
@@ -3,6 +3,7 @@ import { useState, useContext } from "react";
 
 // Radix imports
 import * as Dialog from "@radix-ui/react-dialog";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 // Utility imports
 import { cn } from "../../lib/utils";
@@ -130,21 +131,18 @@ const NewBlog = () => {
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>
-        <button className="rounded-lg bg-green-400 p-2" aria-label="new blog">
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+        <button
+          className={cn(
+            // Base styles
+            "rounded-md bg-green-600 p-1 text-white transition duration-150 ease-in-out",
+            // Hover state
+            "hover:opacity-70",
+            // Active state
+            "active:scale-90 active:opacity-50",
+          )}
+          aria-label="new blog"
+        >
+          <PlusIcon className="size-5" />
         </button>
       </Dialog.Trigger>
       <Dialog.Portal>
